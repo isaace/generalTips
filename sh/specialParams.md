92b73107a2052e47d596308164a5085f2161d39d
Isaac Eliassi <isaac.eliassi@sap.com>
2017-01-21 21:24:37 +0200

    add shell initial file, phase 0.0

---
+++ sh/specialParams.md
@@ -0,0 +1,6 @@
+# General Tips about shell special characters
+*  Argument list ${@}
+*  Switch list of strings to an array
+    * STR_LIST "STR0 STR1 STR2"
+    * str_arr=(STR_LIST)
+*
---
+++ sh/utils.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+#hcp_landscapes
+declare -A SOME_ARR
+HCP_LANDSCAPES_ARR[ind1]="str10 str11 str12 str13"
+HCP_LANDSCAPES_ARR[ind2]="sat20"
+HCP_LANDSCAPES_ARR[ind3]="sat0"
+HCP_LANDSCAPES_ARR[ind4]="sat40"
+
+Useful functions
+#################
+print_and_exec(){
+	echo "exec $1"
+	$1
+	if [ $? -ne 0 ]; then
+	    echo "$1 borked it"
+	fi
+}
+
+#This function makes sure that all the variables at ${MANDATORY_PARAMETERS} is defined
+#Usage:
+#	1. set list of params as strings to ${MANDATORY_PARAMETERS} (i.e. MANDATORY_PARAMETERS="var1 var2")
+#	2. invoke verify_params
+#   -- This job will invoke the print_usage function in case of an error, please implement
+#Example
+#   #List of the mandatory parameters
+#   MANDATORY_PARAMETERS="VAR0 VAR1 VAR2 VAR3"
+#   verify_params
+verify_params() {
+	mandatory_parameters_arr=($MANDATORY_PARAMETERS)
+	for parameter_to_test in "${mandatory_parameters_arr[@]}"
+	do
+		if [ -z ${!parameter_to_test+x} ];
+		then
+			echo "${parameter_to_test} is mandatory and not defined, execution aborted";
+			print_usage
+			exit -1
+		else
+			echo "${parameter_to_test} is mandatory and set to ${!parameter_to_test}"
+		fi
+	done
+}
+
+
+#This function makes take a list of vars and print their values
+#Usage: print_vars_value var1 var2 var3
+print_vars_value() {
+	for parameter_to_test in "${@}"
+	do
+		echo "${parameter_to_test} is equal to ${!parameter_to_test}"
+
+	done
+}
